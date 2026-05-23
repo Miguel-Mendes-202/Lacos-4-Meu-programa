99d27406bee38b90bb49af0f76a5f318df8e14bd
Miguel Mendes <mendesmiguel2007@gmail.com>
2026-05-22 22:47:35 -0300

    Eu fiz

---
+++ Program.cs
@@ -0,0 +1,47 @@
+﻿﻿string ProsseguirProgramaTexto;
+bool ProsseguirPrograma;
+
+Console.WriteLine("Gostaria de Usar este Programa? Digite (Sim/Não) ");
+ProsseguirProgramaTexto = Console.ReadLine()!;
+
+ProsseguirPrograma = ProsseguirProgramaTexto.ToUpper().Substring(0,1) == "Sim";
+
+
+if (ProsseguirProgramaTexto == "Sim")
+{
+string continuar = "s";
+
+while(continuar == "s")
+{
+Console.Write("Qual a Largura? ");
+int Largura = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Qual a Altura? ");
+int Altura = Convert.ToInt32(Console.ReadLine());
+
+string preenchimento = "*";
+
+int contadorLinha = 0;
+while (contadorLinha < Altura)
+{
+    int contadorColuna = 0;
+    while(contadorColuna < Largura)
+    {
+        Console.Write(preenchimento);
+        contadorColuna = contadorColuna + 1;
+    }
+
+    Console.WriteLine();
+    contadorLinha = contadorLinha + 1;
+}
+Console.Write("Deseja executar Novament? (s/n)? ");
+continuar = Console.ReadLine()!.ToLower();
+
+}
+}
+else
+{
+    Console.ForegroundColor = ConsoleColor.Red;
+    Console.WriteLine("Fechando.");
+    Console.ResetColor();
+}
